fd87e702d91659a374275969add2ac0e1a6df143
Diniz Ribeiro <84927857+barrosjunior26@users.noreply.github.com>
2024-10-28 23:46:53 -0300

    Na View Index foi alterado o link de redirecionamento de página. No arquibvo site.js foi incluído duas funções, uma para fechar uma janela do navegador e a outra para exibição e ocultação de um alerta gerado pelo TempData do Razor. Foi realizado a migration para criação do banco de dados e tabelas no banco de dados do SqlServer usando o EntityFramewokCore. Na View Login foi criado a tela de login usando sintaxe de marcação Razor, Html e Bootstrap.

---
+++ Migrations/20241029023558_CriacaoBanco_DriveTech_e_tabelas_tb_login_e_tb_servico.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DriveTech.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriacaoBanco_DriveTech_e_tabelas_tb_login_e_tb_servico : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tb_login",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Senha = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cadastro = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_login", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tb_servico",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Proprietario = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Placa = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Marca = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Modelo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cadastro = table.Column<DateOnly>(type: "date", nullable: false),
+                    Atualizacao = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Observacoes = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Imagem = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    TipoImagem = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_servico", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tb_login");
+
+            migrationBuilder.DropTable(
+                name: "tb_servico");
+        }
+    }
+}
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function echarJanela() {
+    window.close();
+}
+
+setTimeout(function () {
+    $(".alert").fadeOut("slow", function () {
+        $(this).alert("close");
+    });
+}, 5000);
